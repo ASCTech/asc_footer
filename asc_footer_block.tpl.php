cc0cbc1e3a7779de9f42f73c8721e4d37bedabcc
Mike Butsko <butsko.7@osu.edu>
2012-10-30 10:15:14 -0400

    Updates

---
+++ asc_footer_block.tpl.php
@@ -0,0 +1,30 @@
+<p id="footer-address">
+  <strong><a href="/contact">Contact Us</a><strong><br />
+  <?php print $address1 ?><br />
+  <?php print $address2 ?><br />
+  <?php print $city ?> <?php print $state ?>, <?php print $zip ?>
+</p>
+<p id="footer-phone">
+  Phone: <?php print $phone ?><br />
+  Fax: <?php print $fax ?>
+</p>
+<p id="footer-email">
+  E-Mail: <?php print $email ?>
+</p>
+<p id="footer-link-1">
+  E-Mail: <?php print $email ?>
+</p>
+<p id="footer-link-2">
+  E-Mail: <?php print $email ?>
+</p>
+<p id="footer-link-3">
+  E-Mail: <?php print $email ?>
+</p>
+<p id="footer-privacy">
+  <strong><a href="http://artsandsciences.osu.edu/privacy-policy" title="Privacy Policy">Privacy Policy</a></strong><br />
+  <strong><a href="/contact" title="Visit the contact page">Questions, Feedback, Acccessibility</a></strong>
+  <a href="/contact" title="Accessibility">
+    <img src="/profiles/asc/themes/q7/icons/access.png" alt="Accessibility" id="footer-accessibility-icon" width="20" height="20" />
+  </a>
+</p>
+
